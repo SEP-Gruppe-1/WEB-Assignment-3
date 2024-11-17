cc52b6c26593f687b7c97ecee274476b87c0ec8e
Martin <Guffe2010@gmail.com>
2024-11-17 16:42:32 +0100

    commit

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -1,1 +1,32 @@
-//MAKE THE MAGIC HAPPEN
+$(document).ready(function(){
+  $("#watercan").click(function() {
+      $(".waterdrop").show();
+      startDropping();
+  });
+
+  function startDropping() {
+      $(".waterdrop").each(function(index, drop) {
+          setTimeout(function() {
+              animateDrop($(drop));
+          }, index * 300);
+      });
+  }
+
+  function animateDrop($drop) {
+      let startX = 45 + Math.random() * 10; 
+      let startY = 80; 
+      $drop.css({ left: startX + "px", top: startY + "px" });
+
+    
+      $drop.animate({ top: "200px" }, 1000, function() {
+         
+          animateDrop($drop);
+      });
+  }
+});
+
+
+
+
+
+
