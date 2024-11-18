af3255d7bdb8e1f88c10cc87d103055eae830566
VicMarx1 <354405@via.dk>
2024-11-18 12:26:45 +0100

    Wateringcan rotating

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -1,1 +1,17 @@
 //MAKE THE MAGIC HAPPEN
+
+$(document).ready(function(){
+    let rotation = 0;
+    let toggle = true;
+
+    $("#wateringcan").on("click", function (){
+        if (toggle){
+            rotation -= 90;
+        }
+        else{
+            rotation += 90;
+        }
+        $(this).css("transform", `rotate(${rotation}deg)`);
+        toggle = !toggle;
+    })
+})
