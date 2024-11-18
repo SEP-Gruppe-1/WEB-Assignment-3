a3daf22291c2ce90ee20e9c744f757db3ce138d9
VicMarx1 <354405@via.dk>
2024-11-18 12:27:49 +0100

    Wateringcan

--- The Secret Garden - Template/js/script.js
+++ The Secret Garden - Template/js/script.js
@@ -6,12 +6,35 @@ $(document).ready(function(){
 
     $("#wateringcan").on("click", function (){
         if (toggle){
-            rotation -= 90;
+            rotation -= 45;
         }
         else{
-            rotation += 90;
+            rotation += 45;
         }
         $(this).css("transform", `rotate(${rotation}deg)`);
         toggle = !toggle;
     })
 })
+
+function addListeners() {
+    document.getElementById('wateringcan').addEventListener('mousedown', mouseDown, false);
+    window.addEventListener('mouseup', mouseUp, false);
+}
+
+function mouseUp() {
+    window.removeEventListener('mousemove', divMove, true);
+}
+
+function mouseDown() {
+    window.addEventListener('mousemove', divMove, true);
+}
+
+function divMove(e){
+    var div = document.getElementById('wateringcan');
+    div.style.position = 'absolute';
+    div.style.top = e.clientY + 'px';
+    div.style.left = e.clientX + 'px';
+}
+
+
+addListeners();
